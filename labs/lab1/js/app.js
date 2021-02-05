36d7b6dc2ba688eb76e8e85024d1e5749ec0342a
TristanHarvey <harveyta@iu.edu>
2021-02-04 19:40:43 -0500

    Added lab 1

---
+++ labs/lab1/js/app.js
@@ -0,0 +1,20 @@
+function setup() {
+    createCanvas(800,600);
+    noStroke();
+
+    background("#cfd8ab");
+
+    strokeWeight(7);
+
+    stroke(141,12,201);
+    fill(255,204,0);
+    circle(260,90,80);
+
+    fill(111,208,232)
+    rect(200,180,120,80);
+
+    stroke(2);
+    line(200,700,200,260)
+    line(320,700,320,260)
+    
+}
